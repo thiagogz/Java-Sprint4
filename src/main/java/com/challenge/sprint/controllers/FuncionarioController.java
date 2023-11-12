2296a86bb91b67b9f4954b258f2bee0d8d357bc4
fariasao <enzofaarias2010@gmail.com>
2023-11-11 21:11:25 -0300

    cria rotas post, put e delete dos Funcionarios

--- src/main/java/com/challenge/sprint/controllers/FuncionarioController.java
+++ src/main/java/com/challenge/sprint/controllers/FuncionarioController.java
@@ -1,36 +1,52 @@
 package com.challenge.sprint.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.challenge.sprint.entities.Funcionario;
 import com.challenge.sprint.services.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/funcionarios")
 public class FuncionarioController {
-	private final FuncionarioService funcionarioService;
-	
-	@Autowired
-	public FuncionarioController(FuncionarioService funcionarioService) {
+
+    private final FuncionarioService funcionarioService;
+
+    @Autowired
+    public FuncionarioController(FuncionarioService funcionarioService) {
         this.funcionarioService = funcionarioService;
     }
-	
-	@GetMapping
-	public ResponseEntity<List<Funcionario>> findAll() {
-		List<Funcionario> funcionarios = funcionarioService.getAllFuncionario();
-		return ResponseEntity.ok(funcionarios);
-	}
-	
-	@GetMapping(value = "/{id}")
-	public Funcionario findFuncionarioById (@PathVariable Long id) {
-		Funcionario result = funcionarioService.findFuncionarioById(id);
-		return result;
-	}
+
+    @GetMapping
+    public List<Funcionario> getAllFuncionarios() {
+        return funcionarioService.getAllFuncionarios();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Funcionario> getFuncionarioById(@PathVariable Long id) {
+        Funcionario funcionario = funcionarioService.findFuncionarioById(id)
+            .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
+        return ResponseEntity.ok(funcionario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Funcionario> createFuncionario(@RequestBody Funcionario funcionario) {
+        Funcionario novoFuncionario = funcionarioService.saveFuncionario(funcionario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoFuncionario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Funcionario> updateFuncionario(@PathVariable Long id, @RequestBody Funcionario funcionario) {
+        Funcionario funcionarioAtualizado = funcionarioService.updateFuncionario(id, funcionario);
+        return ResponseEntity.ok(funcionarioAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFuncionario(@PathVariable Long id) {
+        funcionarioService.deleteFuncionario(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/challenge/sprint/services/FuncionarioService.java
+++ src/main/java/com/challenge/sprint/services/FuncionarioService.java
@@ -1,29 +1,47 @@
 package com.challenge.sprint.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import com.challenge.sprint.entities.Funcionario;
 import com.challenge.sprint.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FuncionarioService {
-	private final FuncionarioRepository funcionarioRepository;
-	
-	@Autowired
+
+    private final FuncionarioRepository funcionarioRepository;
+
+    @Autowired
     public FuncionarioService(FuncionarioRepository funcionarioRepository) {
         this.funcionarioRepository = funcionarioRepository;
     }
 
-	public List<Funcionario> getAllFuncionario() {
-		return funcionarioRepository.findAll();
-	}
-	
-	public Funcionario findFuncionarioById (@PathVariable Long id) {
-		Funcionario result = funcionarioRepository.findById(id).get();
-		return result;
-	}
+    public List<Funcionario> getAllFuncionarios() {
+        return funcionarioRepository.findAll();
+    }
+
+    public Optional<Funcionario> findFuncionarioById(Long id) {
+        return funcionarioRepository.findById(id);
+    }
+
+    public Funcionario saveFuncionario(Funcionario funcionario) {
+        return funcionarioRepository.save(funcionario);
+    }
+
+    public void deleteFuncionario(Long id) {
+        funcionarioRepository.deleteById(id);
+    }
+
+    public Funcionario updateFuncionario(Long id, Funcionario funcionarioDetails) {
+        Funcionario funcionario = funcionarioRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
+
+        funcionario.setNomeFuncionario(funcionarioDetails.getNomeFuncionario());
+        funcionario.setTelefoneFuncionario(funcionarioDetails.getTelefoneFuncionario());
+        // outros campos conforme necessário
+
+        return funcionarioRepository.save(funcionario);
+    }
 }
