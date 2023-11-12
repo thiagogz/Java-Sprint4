973ebcd14c3c079493128480c7b3e8ebaeaf2131
fariasao <enzofaarias2010@gmail.com>
2023-11-12 20:21:53 -0300

    cria rotas post, put e delete dos tipos de marcas

--- src/main/java/com/challenge/sprint/controllers/MarcaController.java
+++ src/main/java/com/challenge/sprint/controllers/MarcaController.java
@@ -1,36 +1,52 @@
 package com.challenge.sprint.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.challenge.sprint.entities.Marca;
 import com.challenge.sprint.services.MarcaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/marcas")
+@RequestMapping("/marcas")
 public class MarcaController {
-	private final MarcaService marcaService;
-	
-	@Autowired
-	public MarcaController(MarcaService marcaService) {
+
+    private final MarcaService marcaService;
+
+    @Autowired
+    public MarcaController(MarcaService marcaService) {
         this.marcaService = marcaService;
     }
-	
-	@GetMapping
-	public ResponseEntity<List<Marca>> findAll() {
-		List<Marca> marcas = marcaService.getAllMarca();
-		return ResponseEntity.ok(marcas);
-	}
-	
-	@GetMapping(value = "/{id}")
-	public Marca findMarcaById (@PathVariable Long id) {
-		Marca result = marcaService.findMarcaById(id);
-		return result;
-	}
+
+    @GetMapping
+    public List<Marca> getAllMarcas() {
+        return marcaService.getAllMarcas();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Marca> getMarcaById(@PathVariable Long id) {
+        Marca marca = marcaService.findMarcaById(id)
+            .orElseThrow(() -> new RuntimeException("Marca não encontrada"));
+        return ResponseEntity.ok(marca);
+    }
+
+    @PostMapping
+    public ResponseEntity<Marca> createMarca(@RequestBody Marca marca) {
+        Marca novaMarca = marcaService.saveMarca(marca);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaMarca);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Marca> updateMarca(@PathVariable Long id, @RequestBody Marca marca) {
+        Marca marcaAtualizada = marcaService.updateMarca(id, marca);
+        return ResponseEntity.ok(marcaAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMarca(@PathVariable Long id) {
+        marcaService.deleteMarca(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/challenge/sprint/services/MarcaService.java
+++ src/main/java/com/challenge/sprint/services/MarcaService.java
@@ -1,29 +1,46 @@
 package com.challenge.sprint.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import com.challenge.sprint.entities.Marca;
 import com.challenge.sprint.repositories.MarcaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MarcaService {
-	private final MarcaRepository marcaRepository;
-	
-	@Autowired
+
+    private final MarcaRepository marcaRepository;
+
+    @Autowired
     public MarcaService(MarcaRepository marcaRepository) {
         this.marcaRepository = marcaRepository;
     }
 
-	public List<Marca> getAllMarca() {
-		return marcaRepository.findAll();
-	}
-	
-	public Marca findMarcaById (@PathVariable Long id) {
-		Marca result = marcaRepository.findById(id).get();
-		return result;
-	}
+    public List<Marca> getAllMarcas() {
+        return marcaRepository.findAll();
+    }
+
+    public Optional<Marca> findMarcaById(Long id) {
+        return marcaRepository.findById(id);
+    }
+
+    public Marca saveMarca(Marca marca) {
+        return marcaRepository.save(marca);
+    }
+
+    public void deleteMarca(Long id) {
+        marcaRepository.deleteById(id);
+    }
+
+    public Marca updateMarca(Long id, Marca marcaDetails) {
+        Marca marca = marcaRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Marca não encontrada"));
+
+        marca.setMarcaVeiculo(marcaDetails.getMarcaVeiculo());
+        // outros campos conforme necessário
+
+        return marcaRepository.save(marca);
+    }
 }
