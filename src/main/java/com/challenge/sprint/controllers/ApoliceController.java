b350e15364a76b646ebd371289570480b8089d4e
fariasao <enzofaarias2010@gmail.com>
2023-11-12 21:37:08 -0300

    cria rotas post, put e delete das apolices com fks

--- src/main/java/com/challenge/sprint/controllers/ApoliceController.java
+++ src/main/java/com/challenge/sprint/controllers/ApoliceController.java
@@ -1,36 +1,52 @@
 package com.challenge.sprint.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.challenge.sprint.entities.Apolice;
 import com.challenge.sprint.services.ApoliceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/apolices")
+@RequestMapping("/api/apolices")
 public class ApoliceController {
-	private final ApoliceService apoliceService;
-	
-	@Autowired
-	public ApoliceController(ApoliceService apoliceService) {
+
+    private final ApoliceService apoliceService;
+
+    @Autowired
+    public ApoliceController(ApoliceService apoliceService) {
         this.apoliceService = apoliceService;
     }
-	
-	@GetMapping
-	public ResponseEntity<List<Apolice>> findAll() {
-		List<Apolice> apolices = apoliceService.getAllApolice();
-		return ResponseEntity.ok(apolices);
-	}
-	
-	@GetMapping(value = "/{id}")
-	public Apolice findApoliceById (@PathVariable Long id) {
-		Apolice result = apoliceService.findApoliceById(id);
-		return result;
-	}
+
+    @GetMapping
+    public List<Apolice> getAllApolices() {
+        return apoliceService.getAllApolices();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Apolice> getApoliceById(@PathVariable Long id) {
+        Apolice apolice = apoliceService.findApoliceById(id)
+            .orElseThrow(() -> new RuntimeException("Apolice não encontrada"));
+        return ResponseEntity.ok(apolice);
+    }
+
+    @PostMapping
+    public ResponseEntity<Apolice> createApolice(@RequestBody Apolice apolice) {
+        Apolice novaApolice = apoliceService.saveApolice(apolice);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaApolice);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Apolice> updateApolice(@PathVariable Long id, @RequestBody Apolice apolice) {
+        Apolice apoliceAtualizada = apoliceService.updateApolice(id, apolice);
+        return ResponseEntity.ok(apoliceAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteApolice(@PathVariable Long id) {
+        apoliceService.deleteApolice(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/challenge/sprint/services/ApoliceService.java
+++ src/main/java/com/challenge/sprint/services/ApoliceService.java
@@ -1,29 +1,59 @@
 package com.challenge.sprint.services;
 
-import java.util.List;
-
+import com.challenge.sprint.entities.Apolice;
+import com.challenge.sprint.entities.Veiculo;
+import com.challenge.sprint.repositories.ApoliceRepository;
+import com.challenge.sprint.repositories.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import com.challenge.sprint.entities.Apolice;
-import com.challenge.sprint.repositories.ApoliceRepository;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ApoliceService {
-	private final ApoliceRepository apoliceRepository;
-	
-	@Autowired
-    public ApoliceService(ApoliceRepository apoliceRepository) {
+
+    private final ApoliceRepository apoliceRepository;
+    private final VeiculoRepository veiculoRepository;
+
+    @Autowired
+    public ApoliceService(ApoliceRepository apoliceRepository, VeiculoRepository veiculoRepository) {
         this.apoliceRepository = apoliceRepository;
+        this.veiculoRepository = veiculoRepository;
     }
 
-	public List<Apolice> getAllApolice() {
-		return apoliceRepository.findAll();
-	}
-	
-	public Apolice findApoliceById (@PathVariable Long id) {
-		Apolice result = apoliceRepository.findById(id).get();
-		return result;
-	}
+    public List<Apolice> getAllApolices() {
+        return apoliceRepository.findAll();
+    }
+
+    public Optional<Apolice> findApoliceById(Long id) {
+        return apoliceRepository.findById(id);
+    }
+
+    public Apolice saveApolice(Apolice apolice) {
+        validateVeiculo(apolice.getVeiculo());
+        return apoliceRepository.save(apolice);
+    }
+
+    public void deleteApolice(Long id) {
+        apoliceRepository.deleteById(id);
+    }
+
+    public Apolice updateApolice(Long id, Apolice apoliceDetails) {
+        Apolice apolice = apoliceRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Apolice não encontrada"));
+
+        validateVeiculo(apoliceDetails.getVeiculo());
+        apolice.setStatusApolice(apoliceDetails.getStatusApolice());
+        apolice.setValorApolice(apoliceDetails.getValorApolice());
+        apolice.setPlacaVeiculo(apoliceDetails.getPlacaVeiculo());        
+
+        return apoliceRepository.save(apolice);
+    }
+
+    private void validateVeiculo(Veiculo veiculo) {
+        if (veiculo != null && !veiculoRepository.existsById(veiculo.getIdVeiculo())) {
+            throw new RuntimeException("Veículo associado não encontrado");
+        }
+    }
 }
